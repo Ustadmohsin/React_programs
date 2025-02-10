03b9f9008a3c945352d32565677df16db5e8eee3
Ustadmohsin <mohsinustad09@gmail.com>
2025-02-10 16:02:00 +0530

    10feb

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import './new'
+import NewOne from './new'
+
+function App() {
+ 
+  return (
+    <>
+    <NewOne/>
+    <h1>HEADING 1</h1>
+    <p>this is paragrapha</p>
+    </>
+  );
+}
+
+export default App;
---
+++ vite-project/src/new.jsx
@@ -0,0 +1,9 @@
+function NewOne() {
+    return (
+        <>
+            <h1>Hello Chai Aur Code</h1>
+        </>
+    );
+}
+
+export default NewOne;
